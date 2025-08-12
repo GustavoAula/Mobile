3063cf16fc42ae34d2f75444c1e68fa068e5a496
GustavoAula <gustavo.2024014368@alunocesul.com>
2025-08-11 22:13:16 -0300

    02

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,59 @@
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
+import { Product } from "./components/Product"
 
 export function Home() {
+    
+    const products = [
+        "Arroz",
+        "Feijão",
+        "Macarrão",
+        "Farinha de Trigo",
+        "Açúcar",
+        "Sal",
+        "Óleo de Soja",
+        "Leite",
+        "Ovos",
+        "Pão",
+        "Café",
+        "Chá",
+        "Manteiga",
+        "Queijo",
+        "Presunto",
+        "Frango",
+        "Carne Bovina",
+        "Peixe",
+        "Frutas",
+        "Legumes",
+        "Verduras",
+        "Batata",
+        "Cebola",
+        "Alho",
+        "Tomate",
+        "Cenoura",
+        "Banana",
+        "Maçã",
+        "Laranja",
+        "Uva",
+        "Refrigerante",
+        "Suco",
+        "Água Mineral",
+        "Biscoitos",
+        "Cereais",
+        "Molho de Tomate",
+        "Condimentos",
+        "Iogurte",
+        "Sorvete",
+        "Chocolate"
+      ];
+
+    function handleAddProduct(){
+
+    }
+
+    function handleRemoveProduct(name: String){
+        console.log(`Você clicou no botão de Remover Produto $(name)`)
+    }
+
     return(
         <View style={style.container}>
             <Text style={style.tittle}>Lista de Compras</Text>
@@ -11,10 +64,10 @@ export function Home() {
                 style={style.input}
                 placeholder="Nome do Produto"
                 placeholderTextColor="#BDBABA"
-                keyboardType="email-address"
+                keyboardType="default"
                 />
 
-                <TouchableOpacity style={style.button}>
+                <TouchableOpacity style={style.button} onPress={handleAddProduct}>
                     <Text style={style.textButton}>+</Text>
                 </TouchableOpacity>
             </View>
@@ -22,7 +75,12 @@ export function Home() {
             <Text style={style.listTitle}>Compras Pendentes</Text>
             
             <View style={style.list}>
-                <Text style={style.listEmptyText}>Comprou todos os produtos? Adicione produtos a sua lista de compras</Text>
+                {/* <Text style={style.listEmptyText}>Comprou todos os produtos? Adicione produtos a sua lista de compras</Text> */}
+
+            <Product name="Iphone" onRemove={() => handleRemoveProduct("Iphone")} />
+            {
+                products.map()
+            }
             </View>
         </View>
     )
---
+++ src/screens/components/Product.tsx
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+interface Props {
+    name: String;
+    onRemove: () => void;
+}
+
+
+export function Product(props: Props){
+
+    // function handleRemoveProduct(){
+        
+    // }
+
+    // const handleRemoveProduct = () => {
+        
+    // }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.stylesName}>{props.name}</Text>
+
+            <TouchableOpacity 
+            style={styles.button}>
+                <Text style={styles.buttonText}>-</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        // justifyContent: "space-between"
+        alignItems: "center",
+        borderBottomLeftRadius: 5,
+        borderTopLeftRadius: 5,
+    },
+    button: {
+        width: 56,
+        height: 56,
+        backgroundColor: '#E23C44',
+        justifyContent: 'center',
+        alignItems: "center",
+        borderBottomRightRadius: 5,
+        borderTopRightRadius: 5,
+    },
+    buttonText: {
+        fontSize: 24,
+        color: "white",
+    },
+    stylesName:{
+        flex: 1,
+        fontSize: 16,
+        fontWeight: 'normal',
+        lineHeight: 19.2,
+        marginLeft: 13.76,
+        marginRight: 13.76
+    }
+})
